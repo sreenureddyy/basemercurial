0e39bced9afc40cccc3c027547272ede628880d3
sreenureddy.y <sreenureddy.y@localhost>
2012-10-15 23:09:08 +0530

    Workspace clean up for running test case

--- project/src/test/java/com/sree/test/BaseTest.java
+++ project/src/test/java/com/sree/test/BaseTest.java
@@ -3,6 +3,8 @@
  */
 package com.sree.test;
 
+import org.apache.log4j.Logger;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -12,14 +14,20 @@ import com.sree.base.service.IBaseService;
 
 /**
  * @author srinivasr
- *   
+ * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {
-		"/META-INF/spring/springContext-Test.xml",
+@ContextConfiguration(locations = { "/META-INF/spring/springContext-Test.xml",
 		"/META-INF/spring/applicationContext-security.xml",
 		"/META-INF/spring/applicationContext-spring.xml" })
 public class BaseTest {
+	private Logger log = Logger.getLogger(BaseTest.class);
+
 	@Autowired
 	IBaseService baseService;
+
+	@Test
+	public void testInit() {
+		log.info("Initilization of test suit");
+	}
 }
--- project/src/test/java/com/sree/test/TestUser.java
+++ project/src/test/java/com/sree/test/TestUser.java
@@ -3,83 +3,27 @@
  */
 package com.sree.test;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
-import com.sree.base.domain.Address;
-import com.sree.base.domain.ContactDetails;
-import com.sree.base.domain.Country;
-import com.sree.base.domain.LookupValue;
-import com.sree.base.domain.State;
 import com.sree.base.domain.User;
-import com.sree.base.domain.UserAuthority;
-import com.sree.common.utils.CommonUtil;
 
 /**
  * @author srinivasr
  * 
  */
 @SuppressWarnings("unchecked")
-public class TestUser extends BaseTest { 
+public class TestUser extends BaseTest {
 	private Logger log = Logger.getLogger(TestUser.class);
- 
+
 	@Test
 	public void testCase() {
-		List<String> usernames = baseService.find("getusers");
-		log.info(usernames.size());
-		log.info(usernames.get(0));
-		
-		User user = new User();
-		user.setAccountNonExpired(false);
-		user.setAccountNonLocked(false);
-		user.setCreatedBy("admin");
-		user.setCreatedDatetime(new Date());
-		user.setCredentialsNonExpired(false);
-		user.setDob(new Date());
-		user.setEnabled(true);
-		user.setFirstname("sree");
-		user.setGender(1l);
-		user.setLastname("reddy");
-		user.setPassword(CommonUtil.encript("sree"));
-		LookupValue lookupValue = (LookupValue)baseService.find(LookupValue.class, 1l);
-		//user.setUserType(lookupValue);
-		
-		Address address = new Address();
-		address.setAddressType(lookupValue);
-		address.setCityTown("Bangalore");
-		Country country = (Country)baseService.find(Country.class, 1l);
-		address.setCountry(country);
-		address.setCreatedBy("admin");
-		address.setHouseNo("#86");
-		address.setIsPrimary(true);
-		address.setLandmark("Rammaiah Hosp");
-		address.setPinCode("560054");
-		address.setCreatedBy("sree");
-		address.setCreatedDatetime(new Date());
-		State state = (State)baseService.find(State.class, 1l);
-		address.setState(state);
-		address.setStreet("MSR Nager");
-		List<Address> addresses = new ArrayList<Address>();
-		addresses.add(address);
-		user.setAddress(addresses);
-		ContactDetails contactDetails = new ContactDetails();
-		contactDetails.setContact("9886919190");
-		contactDetails.setContactType(lookupValue);
-		contactDetails.setIsPrimaryContact(true);
-		List<ContactDetails> contactDetailsList = new ArrayList<ContactDetails>();
-		contactDetailsList.add(contactDetails);
-		user.setContactDetails(contactDetailsList);
-		
-		UserAuthority authority = (UserAuthority) baseService.find(UserAuthority.class, 1l);
-		List<UserAuthority> userAuthorityList = new ArrayList<UserAuthority>();
-		userAuthorityList.add(authority);
-		user.setUserAuthorities(userAuthorityList);
-		user.setUsername("reddy");
-		baseService.save(user);
-		
+		List<User> users = baseService.find("getAllUsers");
+		log.info(users.size());
+		for (User user : users) {
+			log.info(" Username is :: " + user.getUsername());
+		}
 	}
 }
